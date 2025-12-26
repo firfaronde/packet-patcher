6f99e0eba177d9681eae0a66fe4da95e4405820c
firnoarde <223457997+firfaronde@users.noreply.github.com>
2025-12-27 03:02:27 +0500

    move example

---
+++ example/ClientSnapshotPatch.java
@@ -0,0 +1,22 @@
+package firnoarde.example;
+
+import firnoarde.PNet;
+import mindustry.Vars;
+import mindustry.gen.ClientSnapshotCallPacket;
+
+/**
+ * This patch hides your pointer from other players
+ * */
+public class ClientSnapshotPatch {
+    public static void load() {
+        if(Vars.net instanceof PNet patcher) {
+            patcher.setOutboundOverride(ClientSnapshotCallPacket.class, (packet, send)->{
+                if(packet instanceof ClientSnapshotCallPacket snapshot) {
+                    snapshot.pointerX = 0;
+                    snapshot.pointerY = 0;
+                }
+                send.get(packet, false); // send packet via udp
+            });
+        }
+    }
+}
